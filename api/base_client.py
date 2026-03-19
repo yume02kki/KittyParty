644aad5acdf3c9fe294d36743b007f21f3c602f7
yume02kki <116272426+yume02kki@users.noreply.github.com>
2026-03-19 08:26:46 +0200

    Delete api directory

--- api/base_client.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class BaseApiClient(ABC):
-    @abstractmethod
-    def fetch(self, query: str) -> list[dict]:
-        """Fetch a list of PartyParameters-shaped dicts based on a query string."""
